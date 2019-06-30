82d5be53d7e27623c73dd728e0041519e888a0bb
Piyush Lodaya <ppiyush13@gmail.com>
2019-06-30 12:01:30 +0530

    Reading port from process.env

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const Routes = require('./routes/routes')
 const bodyParser = require('body-parser')
 
 const app = express()
-const port = 13130
+const PORT = process.env.PORT || 13130
+
 
 app.use( bodyParser.json() ); 
 app.get('/', (req, res) => res.send('Health Check'))
 app.use('/api', Routes)
-app.listen(port, () => console.log(`startup-conclave-ms listening on port ${port}!`))
+app.listen(PORT, () => console.log(`startup-conclave-ms listening on port ${PORT}!`))
